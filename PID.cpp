d877c673a1af0ab77b948df21e21de6b86618038
Daniel Wu <danielwuhoo@hotmail.com>
2018-05-29 15:46:34 -0400

    added some PID stuff

--- PID.cpp
+++ PID.cpp
@@ -9,10 +9,24 @@ PID::PID(){
 
 void PID::calculate(char rollError, char pitchError){
 	iRollMem += P_ROLL_GAIN * rollError;
-	if (iRollMem > MAX_ROLL){
-		iRollMem = MAX_ROLL;
+	constrain(iRollMem, MAX_ROLL, MIN_ROLL);
+	rollOut = (P_ROLL_GAIN * rollError) + iRollMem + (D_ROLL_GAIN * (rollError - dRollError));
+	constrain(rollOut, MAX_ROLL, MIN_ROLL);
+	dRollError = rollError;
+
+	iPitchMem += P_PITCH_GAIN * pitchError;
+	constrain(iPitchMem, MAX_PITCH, MIN_PITCH);
+	pitchOut = (P_PITCH_GAIN * pitchError) + iPitchMem + (D_PITCH_GAIN * (pitchError - dPitchError));
+	constrain(rollOut, MAX_ROLL, MIN_ROLL);
+	dPitchError = pitchError;
+
+}
+
+void constrain(char &value, char max, char min){
+	if (value > max){
+		value = max;
 	}
-	else if (iRollMem < MIN_ROLL){
-		iRollMem = MIN_ROLL;
+	else if (value < min){
+		value = min;
 	}
 }
